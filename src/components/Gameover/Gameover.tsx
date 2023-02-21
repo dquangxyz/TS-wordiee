c5542a4b0c39ce28a4e75edbde20d2677407261f
dquangxyz <dq2209@gmail.com>
2023-02-21 11:52:14 +1100

    done testing with gameover component

--- src/components/Gameover/Gameover.tsx
+++ src/components/Gameover/Gameover.tsx
@@ -14,9 +14,7 @@ const Gameover =() => {
             {gameover && attempt <= 6 && <h3>You have successfully found the secret word</h3>}
             {!gameover && attempt === 6 && <h3>Oops, you failed to find out the secret word</h3>}
             {attempt === 6 && <h1>Correct word: {correctWord.toUpperCase()}</h1>}
-            {gameover && <h3>You guessed in {attempt} attempts </h3>}
-            
-            <h1>Correct word: {correctWord.toUpperCase()}</h1>
+            {gameover && <h3>You guessed in {attempt} attempts </h3>}            
         </div>
     )
 }
